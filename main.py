fa86d2b995a02ed5fd2bdcb39848f82d93ff5e02
ElvenVillage <dvolkovdd@gmail.com>
2022-06-03 18:39:21 +0300

    select ROI

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import sys
 import cv2 as cv
 import numpy as np
 
-# Диапазон цветов по HSV модели
-lower = np.array([0, 48, 80], dtype="uint8")
-upper = np.array([20, 255, 255], dtype="uint8")
 
 # OpenCV использует BGR вместо RGB
 red = (0, 0, 255)
@@ -23,25 +20,12 @@ def find_cm(hull):
     return (cx, cy)
 
 
-# Набор точек в 3d - модель руки
-# Пирамида с основанием 720x720 и вершиной в (360, 360, 200)
 points_3d = np.float32([
-    [0.0, 720.0, 0.0],
-    [720.0, 720.0, 0.0],
-    [720.0, 0.0, 0.0],
-    [0.0, 0.0, 0.0],
-    [360.0, 360.0, 360.0]]).reshape(-1, 3)
-
-# Набор точек в 3d - фигура для построения (паралеллипипед 720x720x400 поднят на 100) - над плоскостью
-projection_3d = np.float32([
-    [0.0, 720.0, 500.0],
-    [720.0, 720.0, 500.0],
-    [720.0, 0.0, 500.0],
-    [0.0, 0.0, 500.0],
-    [0.0, 720.0, 100.0],
-    [720.0, 720.0, 100.0],
-    [720.0, 0.0, 100.0],
-    [0.0, 0.0, 100.0]]).reshape(-1, 3)
+    [0.0, 720.0],
+    [720.0, 720.0],
+    [720.0, 0.0],
+    [0.0, 0.0]
+]).reshape(-1, 2)
 
 
 def find_orthogonal(vector):
@@ -56,13 +40,99 @@ def distance(start, end):
     return np.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
 
 
-def process_frame(frame, points_2d_old, points_2d_int_old):
+kernel = np.ones((3, 3), np.uint8)
+
+
+def apply_sensibility(avg_color, newHSens, newSSens, newVSens, maxSensibility):
+    """
+    Applies sensibility values for each value of HSV, taking into account the maximum sensibility possible.
+    It analyses the parameters and executes the hand detection accordingly.
+    Parameters
+    ----------
+    avg_color : array
+      The average of HSV values to be detected
+    newHSens : int
+      Percentage of sensibility to apply to Hue
+    newSSens : int
+      Percentage of sensibility to apply to Saturation
+    newVSens : int
+      Percentage of sensibility to apply to Value
+    maxSensibility : array
+      The maximum error margin of HSV values to be detected
+    """
+    hSens = (newHSens * maxSensibility[0]) / 100
+    SSens = (newSSens * maxSensibility[1]) / 100
+    VSens = (newVSens * maxSensibility[2]) / 100
+    lower_bound_color = np.array(
+        [avg_color[0] - hSens, avg_color[1] - SSens, avg_color[2] - VSens])
+    upper_bound_color = np.array(
+        [avg_color[0] + hSens, avg_color[1] + SSens, avg_color[2] + VSens])
+    return np.array([lower_bound_color, upper_bound_color])
+
+
+def captureCamera(camera):
+    outerRectangleXIni = 300
+    outerRectangleYIni = 50
+    outerRectangleXFin = 550
+    outerRectangleYFin = 300
+    innerRectangleXIni = 400
+    innerRectangleYIni = 150
+    innerRectangleXFin = 450
+    innerRectangleYFin = 200
+
+    while True:
+        _, frame = camera.read()
+        # frame = cv.resize(frame, (0, 0), fx=0.5, fy=0.5)
+        # frame = cv.flip(frame, 1)
+        font = cv.FONT_HERSHEY_SIMPLEX
+        cv.rectangle(frame, (outerRectangleXIni, outerRectangleYIni),
+                     (outerRectangleXFin, outerRectangleYFin), (0, 255, 0), 0)
+        cv.rectangle(frame, (innerRectangleXIni, innerRectangleYIni),
+                     (innerRectangleXFin, innerRectangleYFin), (255, 0, 0), 0)
+        cv.putText(frame, 'Please center your hand in the square', (0, 35),
+                   font, 1, (255, 0, 0), 3, cv.LINE_AA)
+        cv.imshow('Camera', frame)
+
+        key = cv.waitKey(0)
+        if key == ord('v'):
+            roi = frame[innerRectangleYIni +
+                        1:innerRectangleYFin, innerRectangleXIni +
+                        1:innerRectangleXFin]
+            break
+
+    hsvRoi = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
+
+    lower = np.array(
+        [hsvRoi[:, :, 0].min(), hsvRoi[:, :, 1].min(), hsvRoi[:, :, 2].min()])
+    upper = np.array(
+        [hsvRoi[:, :, 0].max(), hsvRoi[:, :, 1].max(), hsvRoi[:, :, 2].max()])
+    h = hsvRoi[:, :, 0]
+    s = hsvRoi[:, :, 1]
+    v = hsvRoi[:, :, 2]
+    hAverage = np.average(h)
+    sAverage = np.average(s)
+    vAverage = np.average(v)
+
+    hMaxSensibility = max(abs(lower[0] - hAverage), abs(upper[0] - hAverage))
+    sMaxSensibility = max(abs(lower[1] - sAverage), abs(upper[1] - sAverage))
+    vMaxSensibility = max(abs(lower[2] - vAverage), abs(upper[2] - vAverage))
+
+    cv.destroyAllWindows()
+
+    return np.array([[hAverage, sAverage, vAverage],
+                     [hMaxSensibility, sMaxSensibility, vMaxSensibility]])
+
+
+def process_frame(frame, points_2d_old, points_2d_int_old, lower, upper):
     contours_dislay_frame = frame.copy()
     hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
     # Фильтруем по цветам, чтобы найти руку
-    hsv_hand = cv.inRange(hsv_frame, lower, upper)
-    _, thresh = cv.threshold(hsv_hand, 170, 255, cv.THRESH_BINARY)
+    thresh = cv.inRange(hsv_frame, lower, upper)
+    thresh = cv.dilate(thresh, kernel, iterations=3)
+    thresh = cv.erode(thresh, kernel, iterations=3)
+    thresh = cv.GaussianBlur(thresh, (5, 5), 90)
+    # _, thresh = cv.threshold(hsv_hand, 170, 255, cv.THRESH_BINARY)
 
     # Ищем контур с наибольшей площадью
     contours, _ = cv.findContours(
@@ -155,7 +225,7 @@ def process_frame(frame, points_2d_old, points_2d_int_old):
             [intersections_1[1][0][0], intersections_1[1][0][1]],
             [intersections_2[1][0][0], intersections_2[1][0][1]],
             [intersections_2[0][0][0], intersections_2[0][0][1]],
-            [center_of_mass[0], center_of_mass[1]]
+
         ])
 
         points_2d_int = np.int32([
@@ -163,82 +233,66 @@ def process_frame(frame, points_2d_old, points_2d_int_old):
             [int(intersections_1[1][0][0]), int(intersections_1[1][0][1])],
             [int(intersections_2[1][0][0]), int(intersections_2[1][0][1])],
             [int(intersections_2[0][0][0]), int(intersections_2[0][0][1])],
-            [center_of_mass[0], center_of_mass[1]]
+
         ])
     else:
         points_2d = points_2d_old
         points_2d_int = points_2d_int_old
 
-    if np.sum(points_2d) != 0:
-        # for i in points_2d:
-        #     cv.circle(contours_dislay_frame, (int(i[0]), int(
-        #         i[1])), color=red, radius=5, thickness=5)
+    if len(points_2d_int) != 0:
 
-        fx = 0.5 + cv.getTrackbarPos('focal', 'contours') / 50.0
         h, w = frame.shape[:2]
-        K = np.float64([[fx*w, 0, 0.5*(w-1)],
-                        [0, fx*w, 0.5*(h-1)],
-                        [0.0, 0.0,      1.0]])
-        dist_coef = np.zeros(4)
-        # M = cv.getPerspectiveTransform(points_3d, points_2d)
-        # d_frame = cv.warpPerspective(tattoo, M, (h, w))
-        # blank_full = np.zeros((h, w, 3), dtype=np.uint8)
-        # blank_black = np.zeros((h, w, 1), dtype=np.uint8)
-        # mask = cv.fillConvexPoly(blank_black, points_2d_int,
-        #  color=255)
-        # mask_inv = cv.bitwise_not(mask)
-        # d_frame = cv.bitwise_and(d_frame, frame, mask=mask)
-        # frame_without_mask = cv.bitwise_or(d_frame, frame, mask=mask)
-        # vis = cv.bitwise_or(d_frame, frame, mask=mask_inv)
-        # d_frame = cv.add(vis, d_frame)
-        # cv.imshow("d_frame", d_frame)
-        ret, rvecs, tvecs = cv.solvePnP(
-            points_3d, points_2d, K, dist_coef, flags=cv.SOLVEPNP_EPNP)
 
-        imgpts, jac = cv.projectPoints(
-            projection_3d, rvecs, tvecs, K, dist_coef)
-        for i in range(-1, len(imgpts)):
-            pt = imgpts[i]
-            pt1 = imgpts[i-1]
-            cv.line(contours_dislay_frame,  (int(pt1[0][0]), int(pt1[0][1])),
-                    (int(pt[0][0]), int(pt[0][1])), color=red, thickness=10)
-        # print(imgpts)
-        # cent = (int(imgpts[0][0][0]), int(
-        # imgpts[0][0][1]))
-        # print(cent)
-        # cv.line(contours_dislay_frame, center_of_mass,
-        # cent, color=red, thickness=4)
-        # cv.circle(contours_dislay_frame,
-        #   (int(imgpts[0][0][0]), int(imgpts[0][0][1])), radius=10, thickness=10, color=blue)
-        # print(imgpts[0][0][0])
-        # cv.circle(contours_dislay_frame,
-        #   (int(imgpts[0][0][0]), int(imgpts[0][0][1])), 5, blue)
+        M = cv.getPerspectiveTransform(points_3d, points_2d)
+        d_frame = cv.warpPerspective(tattoo, M, (h, w))
 
-    # for i in points_2d:
-    #     cv.circle(contours_dislay_frame, i, 5, color=red, thickness=5)
-    # for i in intersections_2:
-    #     cv.circle(contours_dislay_frame, i[0], 5, color=red, thickness=5)
+        blank_black = np.zeros((h, w, 1), dtype=np.uint8)
+        mask = cv.fillConvexPoly(blank_black, points_2d_int,
+                                 color=255)
+        mask_inv = cv.bitwise_not(mask)
+        d_frame = cv.bitwise_and(d_frame, contours_dislay_frame, mask=mask)
+
+        vis = cv.bitwise_or(d_frame, contours_dislay_frame, mask=mask_inv)
+        contours_dislay_frame = cv.add(vis, d_frame)
 
     cv.imshow("contours", contours_dislay_frame)
     return points_2d_int, points_2d
 
 
-def main_loop(filename):
-    video = cv.VideoCapture(filename)
-    points_2d_old = np.float32([0.0, 0.0, 0.0, 0.0, 0.0])
-    points_2d_int_old = np.int32([0, 0, 0, 0, 0])
+def main_loop():
+    camera = cv.VideoCapture(0)
+    points_2d_old = np.float32([0.0, 0.0, 0.0, 0.0])
+    points_2d_int_old = np.int32([0, 0, 0, 0])
+
+    hSensibility = 100
+    sSensibility = 100
+    vSensibility = 100
+
+    avg_color, max_sensibility = captureCamera(camera)
+
+    cv.namedWindow("contours")
+    cv.createTrackbar('HSensb', 'contours', hSensibility, 100, on_trackbar)
+    cv.createTrackbar('SSensb', 'contours', sSensibility, 100, on_trackbar)
+    cv.createTrackbar('VSensb', 'contours',
+                      vSensibility, 100, on_trackbar)
+
     while True:
-        _, frame = video.read()
 
-        if frame is None:
-            break
-        frame = cv.resize(frame, (0, 0), fx=0.5, fy=0.5)
+        _, frame = camera.read()
+        newHSens = cv.getTrackbarPos('HSensb', 'contours')
+        newSSens = cv.getTrackbarPos('SSensb', 'contours')
+        newVSens = cv.getTrackbarPos('VSensb', 'contours')
+        lower_bound_color, upper_bound_color = apply_sensibility(
+            avg_color, newHSens, newSSens, newVSens, max_sensibility)
+
+        # frame = cv.resize(frame, (0, 0), fx=0.5, fy=0.5)
         points_2d_int_old, points_2d_old = process_frame(
-            frame, points_2d_old, points_2d_int_old)
+            frame, points_2d_old, points_2d_int_old, lower_bound_color, upper_bound_color)
 
-        key = cv.waitKey(50)
+        key = cv.waitKey()
         if key == 27:
             cv.destroyAllWindows()
+            camera.release()
             break
 
 
@@ -247,10 +301,5 @@ def on_trackbar(val):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        # main_loop("sample4.mp4")
-        print("Video file is not specified")
-    else:
-        cv.namedWindow("contours")
-        cv.createTrackbar('focal', 'contours', 25, 50, on_trackbar)
-        main_loop(sys.argv[1])
+
+    main_loop()
